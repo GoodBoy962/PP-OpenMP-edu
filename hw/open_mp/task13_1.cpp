6cf46343827285153da349db5997ff8466c17e9b
Alexander Pliskin <alexnetmore@gmail.com>
2017-02-26 23:59:33 +0300

    task 13 updated

--- hw/open_mp/task13_1.cpp
+++ hw/open_mp/task13_1.cpp
@@ -1,16 +1,18 @@
 //
 // Created by Александр Плискин on 16.02.17.
 //
+#include <zconf.h>
 #include "iostream"
 #include "omp.h"
 
 int main() {
-    int thread_id, threads_num;
 #pragma omp parallel num_threads(8)
     {
+        int thread_id, threads_num;
         thread_id = omp_get_thread_num();
         threads_num = omp_get_num_threads();
-        printf("Thread id: %d from %d threads. Hello word!\n",
+        sleep(threads_num - thread_id);
+        printf("Thread id: %d from %d threads. Hello world!\n",
                thread_id + 1, threads_num);
 
     }
