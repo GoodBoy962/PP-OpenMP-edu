0032916e6019e35500e9bebb94e8a41f9de830ef
Alexander Pliskin <alexnetmore@gmail.com>
2017-03-26 15:35:25 +0300

    updates

--- MPI-edu/hw/task2.cpp
+++ MPI-edu/hw/task2.cpp
@@ -1,4 +1,16 @@
 //
 // Created by Александр Плискин on 20.03.17.
 //
+#include "mpi.h"
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+    MPI_Init(NULL, NULL);
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+}
